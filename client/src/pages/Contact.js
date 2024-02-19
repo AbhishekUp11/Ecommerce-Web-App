eb71c25372f1fa12cad8eb506bab4279a534192b
abhishek upadhyay <a.upadhyay@iiitmanipur.ac.in>
2024-02-19 20:28:10 +0530

    Added contact Page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -4,7 +4,9 @@ import Layout from '../components/layout/Layout';
 const Contact = () => {
     return(
       <Layout>
-      <h2> Contact Page </h2>
+      <h2 className='text-center'>Contact Us</h2>
+      <h3 className='text-center'>Email: raghavji@gmail.com</h3>
+      <h3 className='text-center'>Phone: 78654*****</h3>
      </Layout>
     )
 };
